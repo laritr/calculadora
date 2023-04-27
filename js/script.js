75eed2da49d3080c4daa5f4ad711ed1998dd9b4f
Larissa Abreu <124357253+laritr@users.noreply.github.com>
2023-04-27 15:47:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,82 +1,37 @@
+const calcular = document.getElementById('calcular');
 
-function validaEntrada(args) {
-  for (let i = 0; i < arguments.length; i++) {
-    if (!!arguments[i] == false || arguments[i] < 0) {
-      return false;
-    }
-  }
-  return true;
-}
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = +document.getElementById('altura').value;
+    const peso = +document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
-function calcularIMC(kilos, altura) {
-  altura = altura / 100;
-  return kilos / (altura * altura);
-}
+    if (nome !== '' && altura !== '' && peso !== '') {
 
-const formCalcularIMC = document.getElementById('form');
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
 
-formCalcularIMC.addEventListener('submit', function (event) {
-  event.preventDefault();
+        let classificacao = '';
 
-  const kilos = parseFloat(document.getElementById('kilos').value);
-  const altura = parseFloat(document.getElementById('altura').value);
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II';
+        }else {
+            classificacao = 'com obesidade grau III. Cuidado!!';
+        }
 
-  if (validaEntrada(kilos, altura)) {
-    const imc = calcularIMC(kilos, altura);
-    document.getElementById('imc').value = parseFloat(imc).toFixed(2);
-    verificarIMC(imc);
-  } else {
-    document.getElementById('imc').value = '## ERRO ##';
-  }
-});
-
-function verificarIMC(imc) {
-
-  const idade = document.getElementById( 'idade' ).value;
-
-  if (idade >= 18 && idade <= 59) {
-    if (imc < 17) {
-      createMessage('Você está muito abaixo do peso', 'alert');
-    } else if (imc > 17 && imc <= 18.49) {
-      createMessage('Você está abaixo do peso', 'warning');
-    } else if (imc >= 18.5 && imc <= 24.99) {
-      createMessage('Você está com peso normal', 'sucess');
-    } else if (imc >= 25 && imc <= 29.99) {
-      createMessage('Você está acima do peso', 'warning');
-    } else if (imc >= 30 && imc <= 34.99) {
-      createMessage('Você está com obesidade I', 'alert');
-    } else {
-      createMessage('Você está com obesidade II', 'danger');
-    }
-  }
-
-  if (idade >= 60) {
-    if (imc < 22) {
-      createMessage('Abaixo do peso.', 'warning');
-    }else if (imc > 22 && imc <= 27) {
-      createMessage('Adequado ou Eurotrófico', 'sucess');
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        
     }else {
-      createMessage('Sobrepeso', 'alert');
+        resultado.textContent = 'Preencha todos os campos!!!';
     }
-  }
+
 }
 
-function createMessage(msg, type) {
-  document
-    .querySelector('body')
-    .insertAdjacentHTML(
-      'beforebegin',
-      `<div class='message ${type}'>${msg}</div>`
-    );
-
-  setTimeout(function () {
-    deleteMessage();
-  }, 3000);
-}
-
-function deleteMessage() {
-  const list = document.querySelectorAll('.message');
-  for (const item of list) {
-    item.remove();
-  }
-}
+calcular.addEventListener('click', imc);
